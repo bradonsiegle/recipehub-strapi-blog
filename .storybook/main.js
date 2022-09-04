f678d607892c3e6ff238de468e252bf0652ba2d2
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-03 21:48:27 -0600

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+	stories: [
+		'../pages/**/*.stories.mdx',
+		'../pages/**/*.stories.@(js|jsx|ts|tsx)',
+		'../components/**/*.stories.mdx',
+		'../components/**/*.stories.@(js|jsx|ts|tsx)',
+	],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+	],
+	framework: '@storybook/react',
+	core: {
+		builder: '@storybook/builder-webpack5',
+	},
+};
