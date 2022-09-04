ededce467c62d75ebf4d73c8f9566dcdbdc5eeac
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-03 22:05:57 -0600

    add button click test

--- components/Button/Button.test.tsx
+++ components/Button/Button.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import { Button } from './Button';
 
 describe('Button test cases', () => {
@@ -8,5 +10,12 @@ describe('Button test cases', () => {
 		const { asFragment } = render(<Button onClick={onClick}>Test</Button>);
 
 		expect(asFragment()).toMatchSnapshot();
+	});
+	it('should call onClick callback', async () => {
+		const onClick = jest.fn();
+		render(<Button onClick={onClick}>Test</Button>);
+		const button = screen.getByRole('button');
+		await userEvent.click(button);
+		expect(onClick).toHaveBeenCalled();
 	});
 });
