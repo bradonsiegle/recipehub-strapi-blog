824e8ba9c2847bc24cae66b4d16dbd5e087df5d5
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-04 17:30:31 -0600

    global styles

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,6 +2,7 @@ import { MouseEvent } from 'react';
 import styled from '@emotion/styled';
 import { css, SerializedStyles } from '@emotion/react';
 import { AppTheme } from '@/styles/themes';
+import { boxShadow, transition } from '@/components/styles';
 
 export type Color = 'primary' | 'secondary' | 'danger' | 'warning';
 
@@ -45,17 +46,16 @@ export const Button = styled.button<Props>`
 	width: 15rem;
 	height: 4rem;
 	border-radius: 1rem;
-	transition: all 0.4s ease;
+	${transition()};
 	${({ color, theme }) => getColors(theme, color)};
 	${({ theme }) =>
-		`box-shadow: 0.5vmin 0.5vmin 1vmin ${theme.components.shadow1}, -0.5vmin -0.5vmin 1vmin ${theme.components.shadow2}`};
-
+		boxShadow(theme.components.shadow1, theme.components.shadow2)};
 	&:hover {
 		opacity: 0.9;
 	}
 	&:active {
 		${({ theme }) =>
-			`box-shadow: 0.5vmin 0.5vmin 1vmin ${theme.components.shadow1} inset, -0.5vmin -0.5vmin 1vmin ${theme.components.shadow2} inset`}
+			boxShadow(theme.components.shadow1, theme.components.shadow2, true)};
 	}
 `;
 
