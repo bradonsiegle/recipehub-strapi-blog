6dcb8f24739b14bd581c07f010580dc505db6fa9
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-04 19:40:29 -0600

    add styles

--- components/Tile/Tile.tsx
+++ components/Tile/Tile.tsx
@@ -1,12 +1,21 @@
 import { FC } from 'react';
 import styled from '@emotion/styled';
 
+import { boxShadow, borderRadius } from '@/components/styles';
+
 interface TileProps {
 	children?: React.ReactNode;
 	header: string;
 }
 
-const Section = styled.section``;
+const Section = styled.section`
+	${borderRadius};
+	padding: 1vmin 4vmin 4vmin;
+	background: ${({ theme }) => theme.background};
+	color: ${({ theme }) => theme.font.regular};
+	${({ theme }) =>
+		boxShadow(theme.components.shadow1, theme.components.shadow2)};
+`;
 
 export const Tile: FC<TileProps> = ({ header, children }) => {
 	return (
