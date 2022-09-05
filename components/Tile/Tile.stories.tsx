1f62c186903716badc2cfa1193e52f93ac170583
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-04 19:40:42 -0600

    jest tests

--- components/Tile/Tile.stories.tsx
+++ components/Tile/Tile.stories.tsx
@@ -1,4 +1,6 @@
+import { expect } from '@storybook/jest';
 import { ComponentStoryObj, ComponentMeta } from '@storybook/react';
+import { screen } from '@storybook/testing-library';
 
 import { Tile } from './Tile';
 
@@ -8,6 +10,9 @@ export default {
 } as ComponentMeta<typeof Tile>;
 
 export const BasicTile: ComponentStoryObj<typeof Tile> = {
+	play: async ({ args }) => {
+		await expect(screen.getByRole('heading')).toBeInTheDocument();
+	},
 	args: {
 		header: 'Basic Tile',
 		children: 'This is a basic tile',
