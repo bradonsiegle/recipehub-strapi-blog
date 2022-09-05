966519689193753a6ee4d641180895a2b3a4263f
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-04 21:59:12 -0600

    add switch component

---
+++ components/Switch/Switch.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentStoryObj, ComponentMeta } from '@storybook/react';
+import { screen, userEvent } from '@storybook/testing-library';
+
+import { Switch } from './Switch';
+
+export default {
+	title: 'Controls/Switch',
+	component: Switch,
+} as ComponentMeta<typeof Switch>;
+
+export const BasicSwitch: ComponentStoryObj<typeof Switch> = {
+	play: async ({ args }) => {
+		await userEvent.click(screen.getByTestId('SwitchVisiblePart'));
+	},
+};
