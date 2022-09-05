999337a9815e0d35331bd06c94871d455795e63e
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-04 18:28:42 -0600

    refactor button story

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentStoryObj, ComponentMeta } from '@storybook/react';
+import { expect } from '@storybook/jest';
+import { screen, userEvent } from '@storybook/testing-library';
 
 import { Button } from './Button';
 
 export default {
 	title: 'Components/Button',
 	component: Button,
+	args: {
+		children: 'Button',
+	},
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
-
-export const BasicButton = Template.bind({});
-BasicButton.args = {
-	children: 'Button',
+export const PrimaryButton: ComponentStoryObj<typeof Button> = {
+	play: async ({ args }) => {
+		await userEvent.click(screen.getByRole('button'));
+		await expect(args.onClick).toHaveBeenCalled();
+	},
+	args: {
+		color: 'primary',
+	},
 };
