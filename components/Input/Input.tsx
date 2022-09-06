575fafdeee4c8e12e416fb095344ca70bd96a911
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-05 22:08:47 -0600

    refactoring input

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -3,19 +3,14 @@ import styled from '@emotion/styled';
 
 import { Icon, AvailableIcons } from '@/components/Icon';
 import { boxShadow } from '../styles';
-import { useId } from '@/components/hooks/useId';
 
-type StyledInputProps = {
-	withIcon: boolean;
-};
-
-const StyledInput = styled.input<StyledInputProps>`
+const StyledInput = styled.input`
 	all: unset;
-	width: 20rem;
-	height: 4rem;
+	width: ${({ width }) => width}rem;
+	height: ${({ height }) => height}rem;
 	border-radius: 1rem;
 	font-size: 1.4rem;
-	padding-left: ${({ withIcon }) => (withIcon ? 2.8 : 1.4)}rem;
+	padding: 0 3.2rem 0 1.4rem;
 	color: ${({ theme }) => theme.font.regular};
 
 	${({ theme }) =>
@@ -36,47 +31,61 @@ const StyledInput = styled.input<StyledInputProps>`
 `;
 
 const Label = styled.label`
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
 	color: ${({ theme }) => theme.font.regular};
 	font-size: 1rem;
-	padding-left: 1.4rem;
 `;
 
 const StyledIcon = styled(Icon)`
-	display: block;
-	margin-top: -3rem;
-	padding-left: 0.5rem;
+	margin-left: -3rem;
 	color: ${({ theme }) => theme.font.placeholder};
 	opacity: 0.7;
 `;
 
+const InputWrapper = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const Text = styled.span`
+	padding-left: 1.4rem;
+`;
+
 export type Props = {
-	/**Input label */
-	label: string;
 	/**Placeholder */
 	placeholder: string;
 	/**onCHange handler */
 	onChange: ChangeEventHandler<HTMLInputElement>;
-	/**Feedback for input */
-	feedback?: ReactChild;
+	/**Input label */
+	label?: string;
 	/**Icon to show in input */
 	icon?: AvailableIcons;
+	/**Feedback for input */
+	feedback?: ReactChild;
+	/**Input height */
+	height?: string;
+	/**Input width */
+	width?: string;
 };
 
 export const Input: FC<Props & InputHTMLAttributes<HTMLInputElement>> = ({
 	label,
+	height = 4,
+	width = 20,
 	icon,
 	feedback,
 	...props
 }) => {
-	const fieldId = useId();
 	return (
 		<Label>
-			{label}
-			<br />
-			<StyledInput withIcon={Boolean(icon)} id={fieldId} {...props} />
-			{icon && <StyledIcon name={icon} />}
-			<br />
-			<Label>{feedback}</Label>
+			{label && <Text>{label}</Text>}
+			<InputWrapper>
+				<StyledInput height={height} width={width} {...props} />
+				{icon && <StyledIcon name={icon} />}
+			</InputWrapper>
+			{feedback && <Text>{feedback}</Text>}
 		</Label>
 	);
 };
