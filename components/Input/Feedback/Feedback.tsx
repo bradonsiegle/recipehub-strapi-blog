aced8dbbc11467638528f6a153f9449d68a36772
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-05 20:20:51 -0600

    feedback for user input

---
+++ components/Input/Feedback/Feedback.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+
+import styled from '@emotion/styled';
+
+type Props = {
+	isValid?: boolean;
+};
+
+interface FCProps {
+	children: React.ReactNode;
+}
+
+export const Feedback = styled.span<Props>`
+	color: ${({ isValid, theme }) =>
+		isValid ? theme.font.valid : theme.font.invalid};
+`;
+
+export const ConditionalFeedback: FC<FCProps> = ({ children }) =>
+	children ? <Feedback>{children}</Feedback> : <>&nbsp;</>;
