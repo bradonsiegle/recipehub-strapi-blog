4513aeb2d99abd983f4a76b52c1604a515767b2b
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-05 20:59:10 -0600

    refactoring icon

--- components/Icon/Icon.tsx
+++ components/Icon/Icon.tsx
@@ -3,37 +3,20 @@ import { Icons } from './Icons';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 
-export type AvaliableIcons = keyof typeof Icons;
-
-type WrapperProps = {
-	/** Width and Height of icon */
-	size?: number;
-};
+export type AvailableIcons = keyof typeof Icons;
 
 export type Props = {
 	/** Icon name*/
-	name: AvaliableIcons;
-} & WrapperProps &
-	React.SVGProps<SVGSVGElement>;
-
-const Wrapper = styled.div<WrapperProps>`
-	color: ${({ theme }) => theme.font.regular};
-	${({ size }) => {
-		const sizeRem = `${size}rem`;
-		return css`
-			width: ${sizeRem};
-			height: ${sizeRem};
-		`;
-	}}
-`;
+	name: AvailableIcons;
+	/**Width and height */
+	size?: number;
+} & React.SVGProps<SVGSVGElement>;
 
 export const Icon: FC<Props> = ({ name, size = 2, ...rest }) => {
-	const Icon = Icons[name];
+	const Icon = styled(Icons[name])`
+		color: ${({ theme }) => theme.font.regular};
+	`;
 	const sizeInRem = `${size}rem`;
 	const sizes = { width: sizeInRem, height: sizeInRem };
-	return (
-		<Wrapper size={size}>
-			<Icon {...sizes} {...rest} />
-		</Wrapper>
-	);
+	return <Icon {...sizes} {...rest} />;
 };
