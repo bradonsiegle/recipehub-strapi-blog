f566879a4c090ce788e15ab3ebc99513f73ff9ba
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-06 21:09:27 -0600

    Course component

---
+++ components/Course/Course.tsx
@@ -0,0 +1,53 @@
+import { FC } from 'react';
+import Image, { ImageProps } from 'next/image';
+import Link from 'next/link';
+import styled from '@emotion/styled';
+
+import { boxShadow, borderRadius } from '../styles';
+
+const Section = styled.section`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	background: ${({ theme }) => theme.background};
+	color: ${({ theme }) => theme.font.regular};
+	${borderRadius};
+	${({ theme }) =>
+		boxShadow(theme.components.shadow1, theme.components.shadow2)};
+	width: 94vw;
+	@media (min-width: 900px) {
+		width: 46vw;
+	}
+`;
+
+const CourseLink = styled.a`
+	all: unset;
+	cursor: pointer;
+	&:hover {
+		opacity: 0.9;
+	}
+	padding: 1vmin 4vmin;
+`;
+
+interface Props {
+	children: React.ReactNode;
+	/**Header string */
+	header: string;
+	/**Image props */
+	imageProps: ImageProps;
+	/**Link address */
+	link: string;
+}
+
+export const Course: FC<Props> = ({ children, header, link, imageProps }) => (
+	<Section>
+		<Link href={link} passHref>
+			<CourseLink>
+				<h2>{header}</h2>
+				<Image {...imageProps} />
+				{children}
+			</CourseLink>
+		</Link>
+	</Section>
+);
