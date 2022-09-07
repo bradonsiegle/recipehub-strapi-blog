bb9c76f754575c941e0f4fe901f6c83a83d3fd8b
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-06 20:29:43 -0600

    wrap with theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,17 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 import { Layout } from '@/components/Layout/';
+import { ThemeProvider } from '@emotion/react';
+import { Themes } from '@/styles/themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const theme = Themes['dark'];
 	return (
-		<Layout>
-			<Component {...pageProps} />;
-		</Layout>
+		<ThemeProvider theme={theme}>
+			<Layout>
+				<Component {...pageProps} />;
+			</Layout>
+		</ThemeProvider>
 	);
 }
 
