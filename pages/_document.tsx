a0dbc4462c55d2d8104a88ed4c5a8e5be51e6b63
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-06 20:32:13 -0600

    next document

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import { Global } from '@emotion/react';
+
+import { GlobalStyles } from '@/styles/global';
+
+export default function Document() {
+	const setInitialTheme = `
+    var theme = localStorage.getItem("theme");
+    var themeExistsInStorage = Boolean(theme !== null);
+    var isDark = themeExistsInStorage ?
+      Boolean(theme === "dark") :
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+    var backgroundColor = isDark ? "#5e5c64" : "#e4ebf5";
+    var textColor = isDark ? "#E4EBF5e6" : "#504e55e6";
+    document.documentElement.style.setProperty("--themeBackgroundColor", backgroundColor);
+    document.documentElement.style.setProperty("--themeColor", textColor);
+  `;
+	return (
+		<Html>
+			<Head>
+				<link
+					rel='stylesheet'
+					href='https://fonts.googleapis.com/css?family=Poppins|Monoton&display=swap'
+				/>
+				<script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
+				<Global styles={GlobalStyles} />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
