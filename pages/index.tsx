646fb96096918081b48776cb7bb813dbc10069d0
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-07 19:48:21 -0600

    style the courses in the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import styled from '@emotion/styled';
 
 import { Course } from '@/components/Course';
+
+const CoursesWrapper = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	gap: 2vw;
+	margin: 2vh 1vw;
+`;
 
 const Home: NextPage = () => {
 	return (
@@ -11,33 +19,35 @@ const Home: NextPage = () => {
 				<meta name='description' content='IT courses for everyone' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			{Array(4).fill(
-				<Course
-					header='React'
-					link='/hands-on-reactjs'
-					imageProps={{
-						width: 1368,
-						height: 770,
-						alt: 'React',
-						src: '/covers/hands-on_reactjs_cover.png',
-					}}
-				>
-					<>
-						<p>
-							React is the most popular library for building frontend web
-							applications. Step-by-step by diving into all the basics,
-							I&apos;ll introduce you to advanced concepts as well. We&apos;ll
-							build the minesweeper application from scratch We&apos;ll build
-							the minesweeper application from scratch:
-						</p>
-						<ul>
-							<li>setup of the development environment</li>
-							<li>configuration of the React JS app</li>
-							<li>basic algorithms of Minesweeper</li>
-						</ul>
-					</>
-				</Course>
-			)}
+			<CoursesWrapper>
+				{Array(4).fill(
+					<Course
+						header='React'
+						link='/hands-on-reactjs'
+						imageProps={{
+							width: 1368,
+							height: 770,
+							alt: 'React',
+							src: '/covers/hands-on_reactjs_cover.png',
+						}}
+					>
+						<>
+							<p>
+								React is the most popular library for building frontend web
+								applications. Step-by-step by diving into all the basics,
+								I&apos;ll introduce you to advanced concepts as well. We&apos;ll
+								build the minesweeper application from scratch We&apos;ll build
+								the minesweeper application from scratch:
+							</p>
+							<ul>
+								<li>setup of the development environment</li>
+								<li>configuration of the React JS app</li>
+								<li>basic algorithms of Minesweeper</li>
+							</ul>
+						</>
+					</Course>
+				)}
+			</CoursesWrapper>
 		</>
 	);
 };
