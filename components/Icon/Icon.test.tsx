04a3ee57997dd96a8694bcfe2371f16a90aed486
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-15 16:34:14 -0600

    fix import

--- components/Icon/Icon.test.tsx
+++ components/Icon/Icon.test.tsx
@@ -1,9 +1,9 @@
-import { render } from "@testing-library/react";
-import { Icon } from "./Icon";
+import { render } from '@/test-utils';
+import { Icon } from './Icon';
 
-describe("Icon", () => {
-  it("Icon should render", () => {
-    const { asFragment } = render(<Icon name="Moon" />);
-    expect(asFragment()).toMatchSnapshot();
-  });
+describe('Icon test case', () => {
+	it('Icon should render', () => {
+		const { asFragment } = render(<Icon name='Moon' />);
+		expect(asFragment()).toMatchSnapshot();
+	});
 });
