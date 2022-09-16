a604ce3081e04354a6252c0c9482027c7e98dc55
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-15 20:45:58 -0600

    login page structure

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,13 +1,19 @@
 import type { NextPage } from 'next';
 
+import { Tile } from '@/components/Tile';
 import { Input } from '@/components/Input';
+import { Button } from '@/components/Button';
 
 const Login: NextPage = () => {
 	const onSubmit = () => {};
 
 	return (
 		<form onSubmit={onSubmit}>
-			<Input label='Identifier' placeholder='Username or Email'></Input>
+			<Tile header='Login'>
+				<Input label='Identifier' placeholder='Username or Email'></Input>
+				<Input label='Password' type='password' placeholder='Password'></Input>
+				<Button type='submit'>Login</Button>
+			</Tile>
 		</form>
 	);
 };
