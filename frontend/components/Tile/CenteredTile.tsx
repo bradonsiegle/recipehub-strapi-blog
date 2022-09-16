763522dea7536ebbfd151bb799a884c3cd233aa0
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-15 20:57:16 -0600

    add centered version of tile component

---
+++ frontend/components/Tile/CenteredTile.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+import { Tile, TileProps } from './Tile';
+
+const CommonStyles = css`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Wrapper = styled.div`
+	${CommonStyles};
+`;
+
+const StyledTile = styled(Tile)`
+	${CommonStyles};
+	flex-flow: column;
+`;
+
+export const CenteredTile: FC<TileProps> = ({ children, header, ...rest }) => {
+	return (
+		<Wrapper {...rest}>
+			<StyledTile header={header}>{children}</StyledTile>
+		</Wrapper>
+	);
+};
