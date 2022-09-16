03c073e9c2096763e31541e0e8d7cedba1d11b52
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-15 20:42:42 -0600

    make onchange optional

--- frontend/components/Input/Input.tsx
+++ frontend/components/Input/Input.tsx
@@ -1,101 +1,101 @@
-import { FC, ChangeEventHandler, InputHTMLAttributes, ReactChild } from "react";
-import styled from "@emotion/styled";
+import { FC, ChangeEventHandler, InputHTMLAttributes, ReactChild } from 'react';
+import styled from '@emotion/styled';
 
-import { Icon, AvailableIcons } from "@/components/Icon";
-import { boxShadow } from "../styles";
+import { Icon, AvailableIcons } from '@/components/Icon';
+import { boxShadow } from '../styles';
 
 const StyledInput = styled.input`
-  all: unset;
-  width: 100%;
-  height: 100%;
-  border-radius: 1rem;
-  font-size: 1.4rem;
-  padding: 0 3.2rem 0 1.4rem;
-  color: ${({ theme }) => theme.font.regular};
+	all: unset;
+	width: 100%;
+	height: 100%;
+	border-radius: 1rem;
+	font-size: 1.4rem;
+	padding: 0 3.2rem 0 1.4rem;
+	color: ${({ theme }) => theme.font.regular};
 
-  ${({ theme }) =>
-    boxShadow(theme.components.shadow1, theme.components.shadow2, true)};
+	${({ theme }) =>
+		boxShadow(theme.components.shadow1, theme.components.shadow2, true)};
 
-  &::placeholder {
-    color: ${({ theme }) => theme.font.placeholder};
-  }
+	&::placeholder {
+		color: ${({ theme }) => theme.font.placeholder};
+	}
 
-  &:focus {
-    ${({ theme }) =>
-      boxShadow(theme.components.shadow1, theme.components.shadow2)};
-    ~ svg {
-      color: ${({ theme }) => theme.font.regular};
-      opacity: 1;
-    }
-  }
+	&:focus {
+		${({ theme }) =>
+			boxShadow(theme.components.shadow1, theme.components.shadow2)};
+		~ svg {
+			color: ${({ theme }) => theme.font.regular};
+			opacity: 1;
+		}
+	}
 `;
 
 type LabelProps = {
-  /**Input height */
-  height?: number;
-  /**Input width */
-  width?: number;
+	/**Input height */
+	height?: number;
+	/**Input width */
+	width?: number;
 };
 
 const Label = styled.label<LabelProps>`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  width: ${({ width }) => width}rem;
-  height: ${({ height }) => height}rem;
-  color: ${({ theme }) => theme.font.regular};
-  font-size: 1rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	width: ${({ width }) => width}rem;
+	height: ${({ height }) => height}rem;
+	color: ${({ theme }) => theme.font.regular};
+	font-size: 1rem;
 `;
 
 const StyledIcon = styled(Icon)`
-  position: absolute;
-  right: 1rem;
-  color: ${({ theme }) => theme.font.placeholder};
-  opacity: 0.7;
+	position: absolute;
+	right: 1rem;
+	color: ${({ theme }) => theme.font.placeholder};
+	opacity: 0.7;
 `;
 
 const InputWrapper = styled.div`
-  display: flex;
-  position: relative;
-  align-items: center;
-  width: 100%;
-  height: 100%;
+	display: flex;
+	position: relative;
+	align-items: center;
+	width: 100%;
+	height: 100%;
 `;
 
 const Text = styled.span`
-  padding-left: 1.4rem;
+	padding-left: 1.4rem;
 `;
 
 export type Props = {
-  /**Placeholder */
-  placeholder: string;
-  /**onCHange handler */
-  onChange: ChangeEventHandler<HTMLInputElement>;
-  /**Input label */
-  label?: string;
-  /**Icon to show in input */
-  icon?: AvailableIcons;
-  /**Feedback for input */
-  feedback?: ReactChild;
+	/**Placeholder */
+	placeholder: string;
+	/**onCHange handler */
+	onChange?: ChangeEventHandler<HTMLInputElement>;
+	/**Input label */
+	label?: string;
+	/**Icon to show in input */
+	icon?: AvailableIcons;
+	/**Feedback for input */
+	feedback?: ReactChild;
 } & LabelProps;
 
 export const Input: FC<Props & InputHTMLAttributes<HTMLInputElement>> = ({
-  label,
-  height = 7,
-  width = 20,
-  icon,
-  feedback,
-  className,
-  ...props
+	label,
+	height = 7,
+	width = 20,
+	icon,
+	feedback,
+	className,
+	...props
 }) => {
-  return (
-    <Label height={height} width={width} className={className}>
-      {label && <Text>{label}</Text>}
-      <InputWrapper>
-        <StyledInput {...props} />
-        {icon && <StyledIcon name={icon} />}
-      </InputWrapper>
-      {feedback && <Text>{feedback}</Text>}
-    </Label>
-  );
+	return (
+		<Label height={height} width={width} className={className}>
+			{label && <Text>{label}</Text>}
+			<InputWrapper>
+				<StyledInput {...props} />
+				{icon && <StyledIcon name={icon} />}
+			</InputWrapper>
+			{feedback && <Text>{feedback}</Text>}
+		</Label>
+	);
 };
