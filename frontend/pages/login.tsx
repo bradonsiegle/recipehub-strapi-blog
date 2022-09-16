19a14fa8a5f81423f087095edc057667b74ad635
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-15 21:21:59 -0600

    style login

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
+import styled from '@emotion/styled';
 
 import { CenteredTile } from '@/components/Tile';
 import { Input } from '@/components/Input';
 import { Button } from '@/components/Button';
+import { StyledLink } from '@/components/StyledLink';
+
+const StyledInput = styled(Input)`
+	margin-bottom: 1rem;
+`;
 
 const Login: NextPage = () => {
 	const onSubmit = () => {};
@@ -10,9 +17,21 @@ const Login: NextPage = () => {
 	return (
 		<form onSubmit={onSubmit}>
 			<CenteredTile header='Login'>
-				<Input label='Identifier' placeholder='Username or Email'></Input>
-				<Input label='Password' type='password' placeholder='Password'></Input>
+				<StyledInput
+					label='Identifier'
+					placeholder='Username or Email'
+				></StyledInput>
+				<StyledInput
+					label='Password'
+					type='password'
+					placeholder='Password'
+				></StyledInput>
 				<Button type='submit'>Login</Button>
+				<h3>
+					<Link href='/registration' passHref>
+						<StyledLink underline>Create Account</StyledLink>
+					</Link>
+				</h3>
 			</CenteredTile>
 		</form>
 	);
