f9a813967ccd1cf86b7d0d4dec1b29d13a644ceb
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-15 20:57:02 -0600

    change header prop

--- frontend/components/Tile/Tile.tsx
+++ frontend/components/Tile/Tile.tsx
@@ -1,28 +1,28 @@
-import { FC } from "react";
-import styled from "@emotion/styled";
+import { FC, ReactChild } from 'react';
+import styled from '@emotion/styled';
 
-import { boxShadow, borderRadius } from "@/components/styles";
+import { boxShadow, borderRadius } from '@/components/styles';
 
-interface TileProps {
-  children?: React.ReactNode;
-  /**Header String */
-  header: string;
-}
+export type TileProps = {
+	children?: React.ReactNode;
+	/**Header String */
+	header: ReactChild;
+};
 
 const Section = styled.section`
-  ${borderRadius};
-  padding: 1vmin 4vmin 4vmin;
-  background: ${({ theme }) => theme.background};
-  color: ${({ theme }) => theme.font.regular};
-  ${({ theme }) =>
-    boxShadow(theme.components.shadow1, theme.components.shadow2)};
+	${borderRadius};
+	padding: 1vmin 4vmin 4vmin;
+	background: ${({ theme }) => theme.background};
+	color: ${({ theme }) => theme.font.regular};
+	${({ theme }) =>
+		boxShadow(theme.components.shadow1, theme.components.shadow2)};
 `;
 
 export const Tile: FC<TileProps> = ({ header, children }) => {
-  return (
-    <Section>
-      <h2>{header}</h2>
-      {children}
-    </Section>
-  );
+	return (
+		<Section>
+			<h2>{header}</h2>
+			{children}
+		</Section>
+	);
 };
