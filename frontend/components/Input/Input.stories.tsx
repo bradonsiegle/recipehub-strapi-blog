443e8a3286f1bd896bef6dee3de0097a90f00692
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-15 21:15:14 -0600

    refactor input

--- frontend/components/Input/Input.stories.tsx
+++ frontend/components/Input/Input.stories.tsx
@@ -1,58 +1,54 @@
-import { ComponentStoryObj, ComponentMeta } from "@storybook/react";
-import { expect } from "@storybook/jest";
-import { screen, userEvent } from "@storybook/testing-library";
+import { ComponentStoryObj, ComponentMeta } from '@storybook/react';
+import { screen, userEvent } from '@storybook/testing-library';
 
-import { Input } from "./Input";
-import { Feedback } from "./Feedback";
+import { Input } from './Input';
+import { Feedback } from './Feedback';
 
 export default {
-  title: "Controls/Input",
-  component: Input,
-  args: {
-    placeholder: "Your name",
-    label: "Name",
-  },
+	title: 'Controls/Input',
+	component: Input,
+	args: {},
 } as ComponentMeta<typeof Input>;
 
 export const PrimaryInput: ComponentStoryObj<typeof Input> = {
-  play: async ({ args }) => {
-    await userEvent.type(screen.getByRole("textbox"), "String");
-    await expect(args.onChange).toHaveBeenCalledTimes(6);
-  },
-  args: {
-    feedback: "Feedback",
-  },
-};
-
-export const WithValidFeedback: ComponentStoryObj<typeof Input> = {
-  args: {
-    feedback: <Feedback isValid={true}>Looks good!</Feedback>,
-  },
-  argTypes: {
-    feedback: {
-      control: false,
-    },
-  },
-};
-
-export const WithInvalidFeedback: ComponentStoryObj<typeof Input> = {
-  args: {
-    feedback: <Feedback isValid={false}>Required!</Feedback>,
-  },
-  argTypes: {
-    feedback: {
-      control: false,
-    },
-  },
+	play: async ({ args }) => {
+		await userEvent.type(screen.getByRole('textbox'), 'String');
+	},
+	args: {
+		placeholder: 'Your name',
+		label: 'Name:',
+	},
 };
 
 export const WithIcon: ComponentStoryObj<typeof Input> = {
-  args: {
-    icon: "Search",
-  },
-  // argTypes: {
-  // 	icon: {
-  // 		control: false,
-  // 	},
-  // },
+	args: {
+		icon: 'Search',
+		placeholder: 'Search',
+		height: 4,
+	},
+};
+
+export const WithInvalidFeedback: ComponentStoryObj<typeof Input> = {
+	args: {
+		placeholder: 'Some text',
+		feedback: <Feedback isValid={false}>Required!</Feedback>,
+	},
+	argTypes: {
+		feedback: {
+			control: false,
+		},
+	},
+};
+
+export const WithValidFeedback: ComponentStoryObj<typeof Input> = {
+	args: {
+		placeholder: 'Some text',
+		label: 'Text:',
+		feedback: <Feedback isValid={true}>Looks good!</Feedback>,
+	},
+	argTypes: {
+		feedback: {
+			control: false,
+		},
+	},
 };
--- frontend/components/Input/Input.tsx
+++ frontend/components/Input/Input.tsx
@@ -1,60 +1,53 @@
-import { FC, ChangeEventHandler, InputHTMLAttributes, ReactChild } from 'react';
+import {
+	FC,
+	ChangeEventHandler,
+	InputHTMLAttributes,
+	ReactChild,
+	forwardRef,
+	ForwardedRef,
+} from 'react';
 import styled from '@emotion/styled';
 
 import { Icon, AvailableIcons } from '@/components/Icon';
-import { boxShadow } from '../styles';
+import { boxShadow } from '@/components/styles';
 
-const StyledInput = styled.input`
-	all: unset;
-	width: 100%;
-	height: 100%;
-	border-radius: 1rem;
-	font-size: 1.4rem;
-	padding: 0 3.2rem 0 1.4rem;
-	color: ${({ theme }) => theme.font.regular};
-
-	${({ theme }) =>
-		boxShadow(theme.components.shadow1, theme.components.shadow2, true)};
-
-	&::placeholder {
-		color: ${({ theme }) => theme.font.placeholder};
-	}
-
-	&:focus {
-		${({ theme }) =>
-			boxShadow(theme.components.shadow1, theme.components.shadow2)};
-		~ svg {
-			color: ${({ theme }) => theme.font.regular};
-			opacity: 1;
-		}
-	}
-`;
-
-type LabelProps = {
-	/**Input height */
+type WrapperProps = {
+	/** Input height */
 	height?: number;
-	/**Input width */
+	/** Input width */
 	width?: number;
+	/** Label visibilty */
+	isLabelVisible?: boolean;
+	/** Feedback visibilty */
+	isFeedbackVisible?: boolean;
 };
 
-const Label = styled.label<LabelProps>`
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-start;
+const Wrapper = styled.label<WrapperProps>`
+	display: grid;
+	gap: 0.1rem;
+	grid-template-areas:
+		'label'
+		'input'
+		'feedback';
+	grid-template-rows: ${({ isLabelVisible, isFeedbackVisible }) => {
+		if (isLabelVisible && isFeedbackVisible) {
+			return '1fr 3fr 1fr';
+		} else if (isLabelVisible) {
+			return '1fr 4fr 0fr';
+		} else if (isFeedbackVisible) {
+			return '0fr 4fr 1fr';
+		} else {
+			return '0fr 1fr 0fr';
+		}
+	}};
 	width: ${({ width }) => width}rem;
 	height: ${({ height }) => height}rem;
 	color: ${({ theme }) => theme.font.regular};
 	font-size: 1rem;
 `;
 
-const StyledIcon = styled(Icon)`
-	position: absolute;
-	right: 1rem;
-	color: ${({ theme }) => theme.font.placeholder};
-	opacity: 0.7;
-`;
-
 const InputWrapper = styled.div`
+	grid-area: input;
 	display: flex;
 	position: relative;
 	align-items: center;
@@ -62,40 +55,79 @@ const InputWrapper = styled.div`
 	height: 100%;
 `;
 
-const Text = styled.span`
+const StyledInput = styled.input`
+	all: unset;
+	width: 100%;
+	height: 100%;
+	border-radius: 1rem;
+	font-size: 1.4rem;
+	padding: 0 2.6rem 0 1.4rem;
+	color: ${({ theme }) => theme.font.regular};
+	${({ theme }) =>
+		boxShadow(theme.components.shadow1, theme.components.shadow2, true)}
+	&::placeholder {
+		color: ${({ theme }) => theme.font.placeholder};
+	}
+	&:focus {
+		${({ theme }) =>
+			boxShadow(theme.components.shadow1, theme.components.shadow2)}
+		~ svg {
+			color: ${({ theme }) => theme.font.regular};
+			opacity: 1;
+		}
+	}
+`;
+
+const StyledIcon = styled(Icon)`
+	position: absolute;
+	right: 0.3rem;
+	color: ${({ theme }) => theme.font.placeholder};
+	opacity: 0.7;
+`;
+
+const Label = styled.span`
+	grid-area: label;
 	padding-left: 1.4rem;
 `;
 
-export type Props = {
-	/**Placeholder */
-	placeholder: string;
-	/**onCHange handler */
-	onChange?: ChangeEventHandler<HTMLInputElement>;
-	/**Input label */
-	label?: string;
-	/**Icon to show in input */
-	icon?: AvailableIcons;
-	/**Feedback for input */
-	feedback?: ReactChild;
-} & LabelProps;
+const Feedback = styled(Label)`
+	grid-area: feedback;
+`;
 
-export const Input: FC<Props & InputHTMLAttributes<HTMLInputElement>> = ({
-	label,
-	height = 7,
-	width = 20,
-	icon,
-	feedback,
-	className,
-	...props
-}) => {
-	return (
-		<Label height={height} width={width} className={className}>
-			{label && <Text>{label}</Text>}
-			<InputWrapper>
-				<StyledInput {...props} />
-				{icon && <StyledIcon name={icon} />}
-			</InputWrapper>
-			{feedback && <Text>{feedback}</Text>}
-		</Label>
+export type Props = {
+	/** Placeholder  */
+	placeholder: string;
+	/** onChange handler */
+	onChange?: ChangeEventHandler<HTMLInputElement>;
+	/** Input label */
+	label?: string;
+	/** Icon */
+	icon?: AvailableIcons;
+	/** Feedback for input */
+	feedback?: ReactChild;
+} & WrapperProps;
+
+export const Input: FC<Props & InputHTMLAttributes<HTMLInputElement>> =
+	forwardRef(
+		(
+			{ label, height = 7, width = 20, icon, feedback, className, ...rest },
+			ref
+		) => (
+			<Wrapper
+				isLabelVisible={Boolean(label)}
+				isFeedbackVisible={Boolean(feedback)}
+				height={height}
+				width={width}
+				className={className}
+			>
+				<Label>{label}</Label>
+				<InputWrapper>
+					<StyledInput ref={ref as ForwardedRef<HTMLInputElement>} {...rest} />
+					{icon && <StyledIcon name={icon} />}
+				</InputWrapper>
+				<Feedback role='alert'>{feedback}</Feedback>
+			</Wrapper>
+		)
 	);
-};
+
+Input.displayName = 'Input';
