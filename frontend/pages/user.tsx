e7f288d23430287c5ee96985873c2fcfc1d0fe56
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-17 16:37:56 -0600

    add user page

---
+++ frontend/pages/user.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next';
+import { CenteredTile } from '@/components/Tile';
+import { Button } from '@/components/Button';
+
+const User: NextPage = () => {
+	const userMock = {
+		username: 'John Doe',
+		email: 'john@gmail.com',
+	};
+
+	const logoutHandler = async () => {
+		console.log('logout');
+	};
+
+	return (
+		<CenteredTile header='Profile'>
+			<h3>Username: {userMock.username}</h3>
+			<h3>Email: {userMock.email}</h3>
+			<Button onClick={logoutHandler}>Logout</Button>
+		</CenteredTile>
+	);
+};
+
+export default User;
