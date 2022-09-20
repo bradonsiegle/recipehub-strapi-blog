e2455d9320504dc7290e8028c77e5f2eeb54d970
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-19 21:47:07 -0600

    add provider to app

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,16 @@
-import type { AppProps } from "next/app";
-import { Layout } from "@/components/Layout/";
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from '@/store';
+import { Layout } from '@/components/Layout/';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+	return (
+		<Provider store={store}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</Provider>
+	);
 }
 
 export default MyApp;
