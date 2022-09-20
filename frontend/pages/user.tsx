fd6767b531852b9ad4f5a36f9fae8f9b00b108db
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-19 22:01:52 -0600

    add logged in information

--- frontend/pages/user.tsx
+++ frontend/pages/user.tsx
@@ -1,25 +1,40 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { RootState, AppDispatch } from '@/store';
 import { CenteredTile } from '@/components/Tile';
 import { Button } from '@/components/Button';
+import { selectUser, logout } from '@/services/userSlice';
 
 const User: NextPage = () => {
-	const userMock = {
-		username: 'John Doe',
-		email: 'john@gmail.com',
+	const router = useRouter();
+
+	const { username, email, error } = useSelector<RootState, RootState['user']>(
+		selectUser
+	);
+
+	const dispatch = useDispatch<AppDispatch>();
+
+	useEffect(() => {
+		if (!username || Boolean(error)) {
+			dispatch(logout());
+			router.push('/login');
+		}
+	}, []);
+
+	const logoutHandler = () => {
+		dispatch(logout());
+		router.push('/');
 	};
 
-	const logoutHandler = async () => {
-		console.log('logout');
-	};
-
-	return (
+	return username && email ? (
 		<CenteredTile header='Profile'>
-			<h3>Username: {userMock.username}</h3>
-			<h3>Email: {userMock.email}</h3>
+			<h3>Username: {username}</h3>
+			<h3>Email: {email}</h3>
 			<Button onClick={logoutHandler}>Logout</Button>
 		</CenteredTile>
-	);
+	) : null;
 };
 
 export default User;
