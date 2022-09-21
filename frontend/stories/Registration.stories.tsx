6ba507bf4f1ae11a28309b84bd071eb64cb7c58b
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-20 20:28:09 -0600

    add redux

--- frontend/stories/Registration.stories.tsx
+++ frontend/stories/Registration.stories.tsx
@@ -1,8 +1,14 @@
 import Registration from '@/pages/registration';
+import { Provider } from 'react-redux';
+import { store } from '@/store';
 
 export default {
 	title: 'Pages/Registration',
 	component: Registration,
 };
 
-export const RegistrationPage = () => <Registration />;
+export const RegistrationPage = () => (
+	<Provider store={store}>
+		<Registration />
+	</Provider>
+);
