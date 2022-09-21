32c2b2e4d43869e148d724a06a4a51dda4229a2a
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-20 21:17:56 -0600

    add registration tests

--- frontend/pages/registration.tsx
+++ frontend/pages/registration.tsx
@@ -1,118 +1,120 @@
-import type { NextPage } from "next";
-import { useForm } from "react-hook-form";
-import styled from "@emotion/styled";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useSelector, useDispatch } from "react-redux";
+import type { NextPage } from 'next';
+import { useForm } from 'react-hook-form';
+import styled from '@emotion/styled';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { RootState, AppDispatch } from "@/store";
+import { RootState, AppDispatch } from '@/store';
 import {
-  selectUser,
-  registration,
-  RegistrationData,
-} from "@/services/userSlice";
+	selectUser,
+	registration,
+	RegistrationData,
+} from '@/services/userSlice';
 
-import { CenteredTile } from "@/components/Tile";
-import { Input, ConditionalFeedback } from "@/components/Input";
-import { Button } from "@/components/Button";
-import { StyledLink } from "@/components/StyledLink";
+import { CenteredTile } from '@/components/Tile';
+import { Input, ConditionalFeedback } from '@/components/Input';
+import { Button } from '@/components/Button';
+import { StyledLink } from '@/components/StyledLink';
 
 const StyledInput = styled(Input)`
-  margin-bottom: 1rem;
+	margin-bottom: 1rem;
 `;
 
 const Registration: NextPage = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<RegistrationData>();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<RegistrationData>();
 
-  const router = useRouter();
+	const router = useRouter();
 
-  const dispatch = useDispatch<AppDispatch>();
-  const { jwt, error } = useSelector<RootState, RootState["user"]>(selectUser);
+	const dispatch = useDispatch<AppDispatch>();
+	const { jwt, error } = useSelector<RootState, RootState['user']>(selectUser);
 
-  if (Boolean(jwt) && !error) {
-    router.push("/user");
-  }
+	if (Boolean(jwt) && !error) {
+		router.push('/user');
+	}
 
-  const onSubmit = (data: RegistrationData) => {
-    dispatch(registration(data));
-  };
+	const onSubmit = (data: RegistrationData) => {
+		dispatch(registration(data));
+	};
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <CenteredTile header="Create an account">
-        <h3>
-          <ConditionalFeedback>{error?.message}</ConditionalFeedback>
-        </h3>
-        <StyledInput
-          label="username"
-          placeholder="username"
-          feedback={
-            <ConditionalFeedback>
-              {errors.username?.message}
-            </ConditionalFeedback>
-          }
-          {...register("username", {
-            required: "Username is required",
-            minLength: {
-              value: 6,
-              message: "Username must be at least 6 characters long",
-            },
-            pattern: {
-              value: /^[a-zA-Z0-9]+$/,
-              message: "Username must contain only letters and numbers",
-            },
-          })}
-        />
-        <StyledInput
-          label="email"
-          placeholder="email"
-          feedback={
-            <ConditionalFeedback>{errors.email?.message}</ConditionalFeedback>
-          }
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-              message: "Email is invalid",
-            },
-          })}
-        />
-        <StyledInput
-          label="password"
-          placeholder="password"
-          type="password"
-          feedback={
-            <ConditionalFeedback>
-              {errors.password?.message}
-            </ConditionalFeedback>
-          }
-          {...register("password", {
-            required: "Password is required",
-            minLength: {
-              value: 6,
-              message: "Password must be at least 6 characters long",
-            },
-            // pattern: {
-            //     value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
-            //     message:
-            //         'Password must contain at least one uppercase letter, one lowercase letter, and one number',
-            // },
-          })}
-        />
-        <Button type="submit">Register</Button>
-        <h3>
-          Already have an account?{" "}
-          <Link href="/login" passHref>
-            <StyledLink underline>Login</StyledLink>
-          </Link>
-        </h3>
-      </CenteredTile>
-    </form>
-  );
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<CenteredTile header='Create an account'>
+				<h3>
+					<ConditionalFeedback>{error?.message}</ConditionalFeedback>
+				</h3>
+				<StyledInput
+					label='username'
+					placeholder='username'
+					feedback={
+						<ConditionalFeedback>
+							{errors.username?.message}
+						</ConditionalFeedback>
+					}
+					{...register('username', {
+						required: 'Username is required',
+						minLength: {
+							value: 6,
+							message: 'Username must be at least 6 characters long',
+						},
+						pattern: {
+							value: /^[a-zA-Z0-9]+$/,
+							message: 'Username must contain only letters and numbers',
+						},
+					})}
+				/>
+				<StyledInput
+					label='email'
+					placeholder='email'
+					type='email'
+					feedback={
+						<ConditionalFeedback>{errors.email?.message}</ConditionalFeedback>
+					}
+					{...register('email', {
+						required: 'Email is required',
+						pattern: {
+							value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+							message: 'Email is invalid',
+						},
+					})}
+				/>
+				<StyledInput
+					label='password'
+					placeholder='password'
+					type='password'
+					role='textbox'
+					feedback={
+						<ConditionalFeedback>
+							{errors.password?.message}
+						</ConditionalFeedback>
+					}
+					{...register('password', {
+						required: 'Password is required',
+						minLength: {
+							value: 6,
+							message: 'Password must be at least 6 characters long',
+						},
+						// pattern: {
+						//     value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+						//     message:
+						//         'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+						// },
+					})}
+				/>
+				<Button type='submit'>Sign Up</Button>
+				<h3>
+					Already have an account?{' '}
+					<Link href='/login' passHref>
+						<StyledLink underline>Login</StyledLink>
+					</Link>
+				</h3>
+			</CenteredTile>
+		</form>
+	);
 };
 
 export default Registration;
--- frontend/tests/Registration.test.tsx
+++ frontend/tests/Registration.test.tsx
@@ -1,9 +1,149 @@
-import { render } from '@/test-utils';
+import { render, screen, act, waitFor } from '@/test-utils';
+import userEvent from '@testing-library/user-event';
+import { useRouter } from 'next/router';
+
+import { mockUser } from '@/mocks/user';
+
 import Registration from '@/pages/registration';
 
+jest.mock('next/router', () => ({
+	...jest.requireActual('next/router'),
+	useRouter: jest.fn().mockReturnValue({
+		query: {},
+		push: jest.fn(),
+	}),
+}));
+
 describe('Registration page', () => {
-	it('should render', () => {
+	it('Render check', () => {
 		const { container } = render(<Registration />);
 		expect(container).toMatchSnapshot();
+	});
+	it('Client validation check', async () => {
+		render(<Registration />);
+
+		const submitButton = screen.getByRole('button', { name: 'Sign Up' });
+
+		await act(async () => {
+			userEvent.click(submitButton);
+		});
+
+		await act(async () => {
+			userEvent.type(
+				screen.getByRole('textbox', { name: 'username Username is required' }),
+				'test'
+			);
+			userEvent.type(
+				screen.getByRole('textbox', { name: 'email Email is required' }),
+				'test'
+			);
+			userEvent.type(
+				screen.getByRole('textbox', { name: 'password Password is required' }),
+				'test'
+			);
+		});
+
+		expect(
+			screen.getByText('Username must be at least 6 characters long')
+		).toBeInTheDocument();
+		expect(screen.getByText('Email is invalid')).toBeInTheDocument();
+		expect(
+			screen.getByText('Password must be at least 6 characters long')
+		).toBeInTheDocument();
+
+		await act(async () => {
+			userEvent.type(
+				screen.getByRole('textbox', {
+					name: 'username Username must be at least 6 characters long',
+				}),
+				'test--!'
+			);
+			userEvent.type(
+				screen.getByRole('textbox', { name: 'email Email is invalid' }),
+				'test@test'
+			);
+			userEvent.type(
+				screen.getByRole('textbox', {
+					name: 'password Password must be at least 6 characters long',
+				}),
+				'testtest!'
+			);
+		});
+
+		expect(
+			screen.getByText('Username must contain only letters and numbers')
+		).toBeInTheDocument();
+
+		await act(async () => {
+			const usernameInput = screen.getByRole('textbox', {
+				name: 'username Username must contain only letters and numbers',
+			});
+			userEvent.clear(usernameInput);
+			userEvent.type(usernameInput, 'testtesttest');
+		});
+
+		const alerts = screen.getAllByRole('alert');
+
+		expect(alerts).toHaveLength(3);
+		expect(alerts[0]).toMatchSnapshot();
+		expect(alerts[1]).toMatchSnapshot();
+		expect(alerts[2]).toMatchSnapshot();
+	});
+
+	it('Server validation error check', async () => {
+		render(<Registration />);
+
+		const submitButton = screen.getByRole('button', { name: 'Sign Up' });
+
+		act(() => {
+			userEvent.type(
+				screen.getByRole('textbox', { name: 'username' }),
+				'testtest'
+			);
+			userEvent.type(
+				screen.getByRole('textbox', { name: 'email' }),
+				'test@test.test'
+			);
+			userEvent.type(
+				screen.getByRole('textbox', { name: 'password' }),
+				'testtest!'
+			);
+			userEvent.click(submitButton);
+		});
+
+		expect(
+			await screen.findByText('An error occurred during account creation')
+		).toBeInTheDocument();
+	});
+	it('Successful registration check', async () => {
+		// Mock the router
+		const push = jest.fn();
+		(useRouter as jest.Mock).mockReturnValue({
+			query: {},
+			push,
+		});
+
+		render(<Registration />);
+
+		const submitButton = screen.getByRole('button', { name: 'Sign Up' });
+
+		act(() => {
+			userEvent.type(
+				screen.getByRole('textbox', { name: 'username' }),
+				mockUser.user.username
+			);
+			userEvent.type(
+				screen.getByRole('textbox', { name: 'email' }),
+				mockUser.user.email
+			);
+			userEvent.type(
+				screen.getByRole('textbox', { name: 'password' }),
+				mockUser.user.password
+			);
+			userEvent.click(submitButton);
+		});
+
+		// Check if the user is redirected to the user page
+		await waitFor(() => expect(push).toHaveBeenCalled);
 	});
 });
