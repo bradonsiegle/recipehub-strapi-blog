687b2cf1ac86963755a6772755f672daf86cd65e
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-22 18:55:21 -0600

    add aws domain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
-		domains: ['localhost:1337', 'localhost'],
+		domains: [
+			'localhost:1337',
+			'localhost',
+			'ec2-34-204-199-8.compute-1.amazonaws.com',
+		],
 	},
 };
 
