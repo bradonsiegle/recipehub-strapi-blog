d3a16ad864ce5cba9462c47fe8078a01673869b9
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-22 18:57:01 -0600

    linting

--- frontend/components/Button/Button.test.tsx
+++ frontend/components/Button/Button.test.tsx
@@ -1,59 +1,59 @@
-import React from 'react';
-import { render, screen } from '@/test-utils';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import { render, screen } from "@/test-utils";
+import userEvent from "@testing-library/user-event";
 
 import {
-	Button,
-	PrimaryButton,
-	SecondaryButton,
-	DangerButton,
-	WarningButton,
-} from './Button';
+  Button,
+  PrimaryButton,
+  SecondaryButton,
+  DangerButton,
+  WarningButton,
+} from "./Button";
 
-describe('Button test cases', () => {
-	it('should render', () => {
-		const onClick = jest.fn();
-		const { asFragment } = render(<Button onClick={onClick}>Test</Button>);
+describe("Button test cases", () => {
+  it("should render", () => {
+    const onClick = jest.fn();
+    const { asFragment } = render(<Button onClick={onClick}>Test</Button>);
 
-		expect(asFragment()).toMatchSnapshot();
-	});
-	it('should render primary button', () => {
-		const onClick = jest.fn();
-		const { asFragment } = render(
-			<PrimaryButton onClick={onClick}>Test</PrimaryButton>
-		);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it("should render primary button", () => {
+    const onClick = jest.fn();
+    const { asFragment } = render(
+      <PrimaryButton onClick={onClick}>Test</PrimaryButton>
+    );
 
-		expect(asFragment()).toMatchSnapshot();
-	});
-	it('should render secondary button', () => {
-		const onClick = jest.fn();
-		const { asFragment } = render(
-			<SecondaryButton onClick={onClick}>Test</SecondaryButton>
-		);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it("should render secondary button", () => {
+    const onClick = jest.fn();
+    const { asFragment } = render(
+      <SecondaryButton onClick={onClick}>Test</SecondaryButton>
+    );
 
-		expect(asFragment()).toMatchSnapshot();
-	});
-	it('should render danger button', () => {
-		const onClick = jest.fn();
-		const { asFragment } = render(
-			<DangerButton onClick={onClick}>Test</DangerButton>
-		);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it("should render danger button", () => {
+    const onClick = jest.fn();
+    const { asFragment } = render(
+      <DangerButton onClick={onClick}>Test</DangerButton>
+    );
 
-		expect(asFragment()).toMatchSnapshot();
-	});
-	it('should render warning button', () => {
-		const onClick = jest.fn();
-		const { asFragment } = render(
-			<WarningButton onClick={onClick}>Test</WarningButton>
-		);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it("should render warning button", () => {
+    const onClick = jest.fn();
+    const { asFragment } = render(
+      <WarningButton onClick={onClick}>Test</WarningButton>
+    );
 
-		expect(asFragment()).toMatchSnapshot();
-	});
-	it('should call onClick callback', async () => {
-		const onClick = jest.fn();
-		render(<Button onClick={onClick}>Test</Button>);
-		const button = screen.getByRole('button');
-		await userEvent.click(button);
-		expect(onClick).toHaveBeenCalled();
-	});
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it("should call onClick callback", async () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick}>Test</Button>);
+    const button = screen.getByRole("button");
+    await userEvent.click(button);
+    expect(onClick).toHaveBeenCalled();
+  });
 });
--- frontend/components/Course/Course.tsx
+++ frontend/components/Course/Course.tsx
@@ -1,50 +1,50 @@
-import { FC } from 'react';
-import Image, { ImageProps } from 'next/image';
-import Link from 'next/link';
-import styled from '@emotion/styled';
+import { FC } from "react";
+import Image, { ImageProps } from "next/image";
+import Link from "next/link";
+import styled from "@emotion/styled";
 
-import { boxShadow, borderRadius } from '../styles';
-import { StyledLink } from '@/components/StyledLink';
+import { boxShadow, borderRadius } from "../styles";
+import { StyledLink } from "@/components/StyledLink";
 
 const Section = styled.section`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: space-between;
-	padding: 2vmin;
-	background: ${({ theme }) => theme.background};
-	color: ${({ theme }) => theme.font.regular};
-	${borderRadius};
-	${({ theme }) =>
-		boxShadow(theme.components.shadow1, theme.components.shadow2)};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2vmin;
+  background: ${({ theme }) => theme.background};
+  color: ${({ theme }) => theme.font.regular};
+  ${borderRadius};
+  ${({ theme }) =>
+    boxShadow(theme.components.shadow1, theme.components.shadow2)};
 `;
 
 const CourseLink = styled(StyledLink)`
-	display: flex;
-	width: 94vw;
-	@media (min-width: 900px) {
-		width: 46vw;
-	}
+  display: flex;
+  width: 94vw;
+  @media (min-width: 900px) {
+    width: 46vw;
+  }
 `;
 
 interface Props {
-	children: React.ReactNode;
-	/**Header string */
-	header: string;
-	/**Image props */
-	imageProps: ImageProps;
-	/**Link address */
-	link: string;
+  children: React.ReactNode;
+  /**Header string */
+  header: string;
+  /**Image props */
+  imageProps: ImageProps;
+  /**Link address */
+  link: string;
 }
 
 export const Course: FC<Props> = ({ children, header, link, imageProps }) => (
-	<Link href={link} passHref>
-		<CourseLink>
-			<Section>
-				<h2>{header}</h2>
-				<Image {...imageProps} alt={header} />
-				{children}
-			</Section>
-		</CourseLink>
-	</Link>
+  <Link href={link} passHref>
+    <CourseLink>
+      <Section>
+        <h2>{header}</h2>
+        <Image {...imageProps} alt={header} />
+        {children}
+      </Section>
+    </CourseLink>
+  </Link>
 );
