e5fd876e2782780e6d686887799ee0fb2673e4ef
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-26 17:08:20 -0600

    linting

--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
 
 const Search: NextPage = () => {
-	const router = useRouter();
-	const { q } = router.query;
+  const router = useRouter();
+  const { q } = router.query;
 
-	return <h1>Search results for {q}</h1>;
+  return <h1>Search results for {q}</h1>;
 };
 
 export default Search;
