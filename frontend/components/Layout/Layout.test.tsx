4b4da3902d91e8eba3b6d3656d1a10d51b7509a7
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-26 19:27:53 -0600

    add mock router

--- frontend/components/Layout/Layout.test.tsx
+++ frontend/components/Layout/Layout.test.tsx
@@ -1,44 +1,35 @@
 import { render, screen, act } from '@/test-utils';
 import userEvent from '@testing-library/user-event';
+
 import { Layout } from './Layout';
 
 jest.mock('next/router', () => ({
 	...jest.requireActual('next/router'),
-	userRouter: jest.fn().mockReturnValue({
+	useRouter: jest.fn().mockReturnValue({
 		query: {},
 		push: jest.fn(),
 	}),
 }));
 
 describe('Layout test cases', () => {
-	beforeAll(() => {
-		Object.defineProperty(window, 'matchMedia', {
-			writable: true,
-			value: jest.fn().mockImplementation((query) => ({
-				matches: false,
-				media: query,
-				onchange: null,
-				addListener: jest.fn(), // Deprecated
-				removeListener: jest.fn(), // Deprecated
-				addEventListener: jest.fn(),
-				removeEventListener: jest.fn(),
-				dispatchEvent: jest.fn(),
-			})),
-		});
-	});
-
 	const child = (
 		<>
-			<h1>Main Article</h1>
-			<p>Some text</p>
+			<h1>Main article area</h1>
+			<p>
+				In this layout, we display the areas in source order for any screen less
+				that 500 pixels wide. We go to a two column layout, and then to a three
+				column layout by redefining the grid, and the placement of items on the
+				grid.
+			</p>
 		</>
 	);
 
-	it('should render the layout', () => {
-		const { container } = render(<Layout>{child}</Layout>);
-		expect(container).toMatchSnapshot();
+	it('Render check', () => {
+		const { asFragment } = render(<Layout>{child}</Layout>);
+
+		expect(asFragment()).toMatchSnapshot();
 	});
-	it('should toggle theme', async () => {
+	it('Theme toggle check', async () => {
 		localStorage.setItem('theme', 'light');
 		(window.matchMedia as jest.Mock).mockReturnValue({ matches: true });
 
