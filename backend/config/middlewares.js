715b765a8596db1bf55ea0e25ad2eda92a6f4b95
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-30 14:16:14 -0600

    fix middleware to allow cloudinary

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,12 +1,20 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "img-src": ["'self'", "data:", "strapi.io", "res.cloudinary.com/"],
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::favicon",
+  "strapi::public",
 ];
