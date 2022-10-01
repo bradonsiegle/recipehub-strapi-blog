539872ac255a8289cb53b88dbf66ac380bf00ad4
Bradon Siegle <bradonsiegle@gmail.com>
2022-09-30 21:03:32 -0600

    linting

--- frontend/components/Tile/Tile.tsx
+++ frontend/components/Tile/Tile.tsx
@@ -15,7 +15,6 @@ const Section = styled.section`
   max-width: 90vw;
   padding: 2vmin 4vmin 4vmin;
   background: ${({ theme }) => theme.background};
-
   color: ${({ theme }) => theme.font.regular};
   ${({ theme }) =>
     boxShadow(theme.components.shadow1, theme.components.shadow2)};
