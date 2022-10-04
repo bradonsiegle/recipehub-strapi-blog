2606e4840cb1ca14cb6eb799473a8c1c847e1e27
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-03 21:47:42 -0600

    add new styling

--- frontend/pages/user.tsx
+++ frontend/pages/user.tsx
@@ -7,6 +7,9 @@ import { CenteredTile } from "@/components/Tile";
 import { Button } from "@/components/Button";
 import { selectUser, logout } from "@/services/userSlice";
 
+import { FaRegSmileBeam } from "react-icons/fa";
+import styled from "@emotion/styled";
+
 const User: NextPage = () => {
   const router = useRouter();
 
@@ -15,6 +18,12 @@ const User: NextPage = () => {
   const { username, email, error } = useSelector<RootState, RootState["user"]>(
     selectUser
   );
+
+  const StyledHeader = styled.h2`
+    font-family: "Playfair Display", serif;
+    text-align: center;
+    margin-bottom: 10vmin;
+  `;
 
   useEffect(() => {
     if (!username || Boolean(error)) {
@@ -29,11 +38,22 @@ const User: NextPage = () => {
   };
 
   return username && email ? (
-    <CenteredTile header="Profile">
-      <h3>Username: {username}</h3>
-      <h3>Email: {email}</h3>
-      <Button onClick={logoutHandler}>Logout</Button>
-    </CenteredTile>
+    <>
+      <StyledHeader>
+        Bon Appétit, {username}{" "}
+        <FaRegSmileBeam
+          size={22}
+          style={{ position: "relative", top: "3px" }}
+        />
+      </StyledHeader>
+      <CenteredTile header="Profile">
+        <h3>Username: {username}</h3>
+        <h3>Email: {email}</h3>
+        <Button onClick={logoutHandler} style={{ margin: "2rem 0" }}>
+          Logout
+        </Button>
+      </CenteredTile>
+    </>
   ) : null;
 };
 
