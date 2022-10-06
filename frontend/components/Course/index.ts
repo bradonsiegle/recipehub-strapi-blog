ab61fefe108a9ba0ae139e82b3c03f6afb7bb863
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-06 14:10:07 -0600

    add additional exports

--- frontend/components/Course/index.ts
+++ frontend/components/Course/index.ts
@@ -1,2 +1,10 @@
-export { Course, Wrapper, Courses } from "./Course";
+export {
+  Course,
+  Wrapper,
+  Courses,
+  CourseLink,
+  Section,
+  ImageContainer,
+} from "./Course";
 export { FeaturedRecipe } from "./FeaturedRecipe";
+export { CourseCategoryTile } from "./CourseCategoryTile";
