53d59481a77489cf5fd979428fd6e05b8e75de1a
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-06 14:10:33 -0600

    add course category tiles to the index page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,11 +2,19 @@ import type { NextPage, GetStaticProps } from "next";
 import Head from "next/head";
 import { Course as CourseType, Response } from "@/types";
 import styled from "@emotion/styled";
-import { Courses } from "@/components/Course";
-import { CenteredTile } from "@/components/Tile";
+import { CourseCategoryTile, Courses } from "@/components/Course";
 import { FeaturedRecipe } from "@/components/Course";
 
+import { useRouter } from "next/router";
+
 type CoursesResponse = Response<CourseType[]>;
+
+const OrderDiv = styled.div<{ order: number; largeDeviceOrder: number }>`
+  order: ${({ order }) => order};
+  @media (min-width: 768px) {
+    order: ${({ largeDeviceOrder }) => largeDeviceOrder};
+  }
+`;
 
 const StyledHeader = styled.h2`
   font-family: "Playfair Display", serif;
@@ -25,9 +33,13 @@ const Subtitle = styled.h3`
 
 const StyledDiv = styled.div`
   display: flex;
+  gap: 2rem;
   justify-content: center;
   align-items: center;
   flex-flow: column;
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -68,34 +80,66 @@ const strapi_url = process.env.NEXT_PUBLIC_STRAPI_URL;
 const Home: NextPage<{ courses: CourseType[]; featuredCourse: CourseType }> = ({
   courses,
   featuredCourse,
-}) => (
-  <>
-    <Head>
-      <title>RecipeHub</title>
-      <meta name="description" content="IT courses for everyone" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+}) => {
+  const router = useRouter();
+  return (
+    <>
+      <Head>
+        <title>RecipeHub</title>
+        <meta name="description" content="IT courses for everyone" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <StyledDiv>
-      <FeaturedRecipe
-        header={featuredCourse.attributes.header}
-        link={`/course/${featuredCourse.id}`}
-        imageProps={{
-          width: `${featuredCourse.attributes.cover.data.attributes.formats.medium.width}`,
-          height: `${featuredCourse.attributes.cover.data.attributes.formats.medium.height}`,
-          src: `${strapi_url}${featuredCourse.attributes.cover.data.attributes.formats.medium.url}`,
-          alt: `Cover for ${featuredCourse.attributes.header}`,
-          style: { borderRadius: "1rem" },
-        }}
-      >
-        <Subtitle>{featuredCourse.attributes.subtitle}</Subtitle>
-      </FeaturedRecipe>
-    </StyledDiv>
-    <hr style={{ marginBottom: "2rem" }} className="responsive" />
+      <StyledDiv>
+        <OrderDiv order={2} largeDeviceOrder={1}>
+          <CourseCategoryTile
+            header={"Need dinner ideas? We've got you covered!"}
+            link="/search?q=dinner"
+            imageProps={{
+              src: "https://res.cloudinary.com/dxpt2kzgn/image/upload/v1665080425/images/caprese-chicken-saltimbocca1-1658266242.jpg_wtdpph.jpg",
+              height: 260,
+              width: 390,
+              style: { borderRadius: "0.5rem" },
+            }}
+          />
+        </OrderDiv>
+        <OrderDiv order={1} largeDeviceOrder={2}>
+          <FeaturedRecipe
+            header={featuredCourse.attributes.header}
+            link={`/course/${featuredCourse.id}`}
+            imageProps={{
+              width: `${featuredCourse.attributes.cover.data.attributes.formats.medium.width}`,
+              height: `${featuredCourse.attributes.cover.data.attributes.formats.medium.height}`,
+              src: `${strapi_url}${featuredCourse.attributes.cover.data.attributes.formats.medium.url}`,
+              alt: `Cover for ${featuredCourse.attributes.header}`,
+              style: { borderRadius: "1rem" },
+            }}
+          >
+            <Subtitle>{featuredCourse.attributes.subtitle}</Subtitle>
+          </FeaturedRecipe>
+        </OrderDiv>
+        <OrderDiv order={3} largeDeviceOrder={3}>
+          <CourseCategoryTile
+            header={"Explore easy-to-make cocktails! Home mixology 101."}
+            link="/search?q=cocktail"
+            imageProps={{
+              src: "https://res.cloudinary.com/dxpt2kzgn/image/upload/v1665080521/images/summer-cocktails-24374-3_ixzrac.jpg",
+              height: 260,
+              width: 390,
+              style: { borderRadius: "0.5rem" },
+            }}
+          />
+        </OrderDiv>
+      </StyledDiv>
+      <hr
+        style={{ marginTop: "2rem", marginBottom: "2rem" }}
+        className="responsive"
+      />
 
-    <StyledHeader>Our lastest recipes</StyledHeader>
-    <Courses courses={courses.reverse()} strapi_url={String(strapi_url)} />
-  </>
-);
+      <StyledHeader>Our lastest recipes</StyledHeader>
+      <Courses courses={courses.reverse()} strapi_url={String(strapi_url)} />
+    </>
+  );
+};
 
 export default Home;
